a8453e68e0fd4f8f4379f49116e1f39de99d0a2a
Pawel Sawicki <pwlsawicki@gmail.com>
2025-02-06 11:45:08 +0100

    BookListItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { AppHeader } from './components/AppHeader';
+import { BookListItem } from './components/BookListItem';
+import { exampleBooks } from './domain/book';
 
 function App() {
   return (
     <>
       <div className="App">
         <AppHeader />
+        <BookListItem book={exampleBooks[0]} />
+        <BookListItem book={exampleBooks[1]} />
+        <BookListItem book={exampleBooks[2]} />
       </div>
     </>
   );
